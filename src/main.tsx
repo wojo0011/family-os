d3b47ee38f33200730ffdc6cc4eef3d9d74457d7
Wojtek Matwiejczyk <corewebmedia@gmail.com>
2026-08-17 07:38:49 -0400

    Enable textured Earth and Mars moon-base overlay

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import { installWeatherIconCompatibility } from './weatherIconCompatibility';
 import { installSpaceThemeDefault } from './spaceThemeDefault';
 import { installThemeFavicon } from './themeFavicon';
 import { installSpaceExperience } from './spaceExperience';
+import { installSpacePlanetExperience } from './spacePlanetExperience';
 import './styles.css';
 import './enhancements.css';
 import './weather.css';
 import './space-today.css';
 import './space-experience.css';
+import './space-planets.css';
 
 installWeatherIconCompatibility();
 installSpaceThemeDefault();
 installThemeFavicon();
 installSpaceExperience();
+installSpacePlanetExperience();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
