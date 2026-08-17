2b9d7a78f65ee21ee4b9449255ec7fa759db06e9
Wojtek Matwiejczyk <corewebmedia@gmail.com>
2026-08-16 23:05:10 -0400

    Activate Space as default and load Today space scene

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { installWeatherIconCompatibility } from './weatherIconCompatibility';
+import { installSpaceThemeDefault } from './spaceThemeDefault';
 import './styles.css';
 import './enhancements.css';
 import './weather.css';
+import './space-today.css';
 
 installWeatherIconCompatibility();
+installSpaceThemeDefault();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
