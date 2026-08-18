f0bc997c89c2e5fc681cecf9e1ab18d123c4d170
Wojtek Matwiejczyk <corewebmedia@gmail.com>
2026-08-18 07:54:41 -0400

    Remove obsolete capture interaction workaround

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ import { installMidnight3DLauncher } from './midnight3DLauncher';
 import { installNature3DLauncher } from './nature3DLauncher';
 import { installSoft3DLauncher } from './soft3DLauncher';
 import { installCaptureModalController } from './captureModalController';
-import { installCaptureModalInteractionFix } from './captureModalInteractionFix';
 import './styles.css';
 import './enhancements.css';
 import './weather.css';
@@ -24,7 +23,6 @@ installMidnight3DLauncher();
 installNature3DLauncher();
 installSoft3DLauncher();
 installCaptureModalController();
-installCaptureModalInteractionFix();
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Family OS root element is missing.');
